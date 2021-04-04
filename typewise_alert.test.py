fb942745c61b80c304ca41bbba761feba0ffddfa
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-04-04 17:03:22 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,26 +1,20 @@
 import unittest
 import typewise_alert
 
+
 class TypewiseTest(unittest.TestCase):
-  def test_breach_type_normal(self):
-    breachType = typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 34)
-    self.assertTrue(breachType == "NORMAL")
-    typewise_alert.check_and_alert('email',breachType)
-    
-  def test_breach_type_too_high(self):
-    breachType = typewise_alert.classify_temperature_breach('HIGH_ACTIVE_COOLING', 46)
-    self.assertTrue(breachType == "TOO_HIGH")
-    typewise_alert.check_and_alert('controller',breachType)  
+  def test_breach_type(self):
+    self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 34) == "NORMAL")
+    self.assertTrue(typewise_alert.classify_temperature_breach('HIGH_ACTIVE_COOLING',90) == "TOO_HIGH")
+    self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', -10) == "TOO_LOW")
     
   def test_improper_input(self):
-    breachType = typewise_alert.classify_temperature_breach('liquid_coolant', 36)
-    self.assertTrue(breachType == "Improper Input")
-    typewise_alert.check_and_alert('controller',breachType)
+    self.assertTrue(typewise_alert.classify_temperature_breach('liquid_coolant', 36)== "Improper Input")
+    self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', None)== "Improper Input")
     
-  def test_improper_value(self):
-    breachType = typewise_alert.classify_temperature_breach('PASSIVE_COOLING', None)
-    self.assertTrue(breachType == "Improper Input")
-    typewise_alert.check_and_alert('email',breachType)
+  def test_check_and_alert(self):
+    self.assertTrue(typewise_alert.check_and_alert('console',{'coolingType':'PASSIVE_COOLING'},70)== True)
+    self.assertTrue(typewise_alert.check_and_alert('email',{'coolingType':'PASSIVE_COOLING'},70)== True)
 
 if __name__ == '__main__':
   unittest.main()
