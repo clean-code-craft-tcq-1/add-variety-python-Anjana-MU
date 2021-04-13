ddc4025161ad8c44ea06a8ca64ce64afcc2d5d92
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-04-13 20:39:36 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -10,11 +10,14 @@ class TypewiseTest(unittest.TestCase):
     
   def test_improper_input(self):
     self.assertTrue(typewise_alert.classify_temperature_breach('liquid_coolant', 36)== "Improper Input")
+    self.assertTrue(typewise_alert.classify_temperature_breach('', None)== "Improper Input")
     self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', None)== "Improper Input")
     
   def test_check_and_alert(self):
     self.assertTrue(typewise_alert.check_and_alert('console',{'coolingType':'PASSIVE_COOLING'},70)== True)
     self.assertTrue(typewise_alert.check_and_alert('email',{'coolingType':'PASSIVE_COOLING'},70)== True)
+    self.assertTrue(typewise_alert.check_and_alert('pdf',{'coolingType':'PASSIVE_COOLING'},70)== 'Improper Input')
+    self.assertTrue(typewise_alert.check_and_alert(' ',{'coolingType':'PASSIVE_COOLING'},70)== 'Improper Input')
 
 if __name__ == '__main__':
   unittest.main()
